18051d50aedb42e1e5ed39875ea5f7573977f215
Yasmim <ymdsc.coimbra85@gmail.com>
2025-03-08 22:10:02 -0300

    adding a function to store the email input value

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -1,31 +1,36 @@
 const emailInput = document.querySelector('input[type="email"]');
-const errorMsg = document.querySelector('.error-msg');
-const formBtn = document.querySelector('button');
+const errorMsg = document.querySelector(".error-msg");
+const formBtn = document.querySelector("button");
 
 let emailValido = false;
 
-formBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+formBtn.addEventListener("click", (event) => {
+	event.preventDefault();
 
-    validateEmail();
-    
-    if (emailValido) {
-        window.location.replace('./success-page.html');
-    } else {
-        addErrorState();
-    }
+	validateEmail();
+
+	if (emailValido) {
+		window.location.replace("./success-page.html");
+	} else {
+		addErrorState();
+	}
 });
 
 function validateEmail() {
-    const email = emailInput.value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-    if (emailRegex.test(email)) {
-        emailValido = true;
-    }
+	const email = emailInput.value;
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+	if (emailRegex.test(email)) {
+		emailValido = true;
+	}
 }
 
 function addErrorState() {
-    errorMsg.classList.add('ativo');
-    emailInput.classList.add('invalid');
+	errorMsg.classList.add("ativo");
+	emailInput.classList.add("invalid");
+}
+
+export function storeEmail() {
+    const email = localStorage.getItem("email");
+    return email;
 }
